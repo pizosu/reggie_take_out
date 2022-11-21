7b42743ac32d54149cff20983d70bce08dc4ded7
suzhensheng <2661966364@qq.com>
2022-11-21 11:47:42 +0800

    使用Sharding-jdbc实现程序的读写分离

--- src/test/java/com/example/reggie_take_out/ReggieTakeOutApplicationTests.java
+++ src/test/java/com/example/reggie_take_out/ReggieTakeOutApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.reggie_take_out;
 
+import ch.qos.logback.classic.spi.ClassPackagingData;
+import com.example.reggie_take_out.service.AddressBookService;
+import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
